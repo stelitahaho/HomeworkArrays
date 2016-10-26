f60388c00b89e684ac413d5107815ec5dad4939e
Stela <Stela@DESKTOP-KFO5BUG>
2016-10-26 10:06:39 +0300

    Sum even elements in an array

---
+++ src/SumEvenNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class SumEvenNumbers {
+
+	public static void main(String[] args) {	
+			Scanner input = new Scanner(System.in, "UTF-8");
+			System.out.println("Enter Array size :");
+			int n=input.nextInt();
+			int[] Array= new int [n];
+			int arr;
+			int Sum=0;
+			for(int i=0; i < Array.length ; i++ ){
+				{
+			   Array[i]=arr=input.nextInt();
+				}
+			}
+			for(int i=0 ; i < Array.length ; i++){
+				if(Array[i]%2!=0){
+					Sum+=Array[i];
+				}
+			}
+			System.out.print("The Sum of the even numbers is : " +Sum);
+			input.close();
+		}
+
+	}
